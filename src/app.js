6ccd64acbe522d4fd0561cc31d8cf9da3e17d06a
Dariet-SP <159501959+Dariet-SP@users.noreply.github.com>
2024-08-01 16:00:42 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,39 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const generateExcuseButton = document.getElementById("generate-excuse");
+  const excuseParagraph = document.getElementById("excuse");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let excuses = [];
+
+  // Genera las excusas
+  for (let i = 0; i < who.length; i++) {
+    for (let j = 0; j < action.length; j++) {
+      for (let k = 0; k < what.length; k++) {
+        for (let l = 0; l < when.length; l++) {
+          let domain = `${who[i]} ${action[j]} ${what[k]} ${when[l]}`;
+          excuses.push(domain);
+        }
+      }
+    }
+  }
+
+  // Función para generar una excusa aleatoria
+  function generateExcuse() {
+    const randomIndex = Math.floor(Math.random() * excuses.length);
+    const randomExcuse = excuses[randomIndex];
+    excuseParagraph.innerHTML = randomExcuse;
+  }
+
+  // Asocia el evento click al botón
+  generateExcuseButton.addEventListener("click", generateExcuse);
 };
